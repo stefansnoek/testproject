83224573a3531f368b4d4f7d4203e7df4a51c11b
stefansnoek <60693056+stefansnoek@users.noreply.github.com>
2025-03-09 17:06:15 +0100

    test

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "subscription_id" {
+  description = "The Azure subscription ID"
+}
+
+variable "tenant_id" {
+  description = "The Azure tenant ID"
+}
+
+variable "client_id" {
+  description = "The Azure client ID"
+}
+
+variable "client_secret" {
+  description = "The Azure client secret"
+}
